1d149c9a327fd484f7c4e0abbc50935302d4b618
angelo234 <angmatt90@gmail.com>
2023-09-26 10:51:27 -0700

    Properly putting functions in '__builtins__' "context" variable. If variable doesn't exist, now it won't throw an error. Tried to override thruthiness of values zero and '' to match Lua, but wasn't successful. Added more test cases.

--- jbeam_editor/expr_parser.py
+++ jbeam_editor/expr_parser.py
@@ -19,27 +19,47 @@
 # SOFTWARE.
 
 import math
+import random
 import re
 import sys
 
+var_wrapper_re = re.compile(r'\$([a-zA-Z_][a-zA-Z_0-9]*)')
 standalone_equal_re = re.compile(r'[^<>!=]=[^=]')
+
+
+'''class Truth:
+    def __init__(self, value, truth):
+        self.value = value
+        self.truth = truth
+    def __bool__(self):
+        return self.truth'''
 
 
 # function used as a case selector, input can be both int and bool as the first argument, any number of arguments after that
 # in case it's a bool, it works like a ternary if, returning the second param if true, the third if false
 # if the selector is an int n, it simply returns the nth+1 param it was given, if n > #params it returns the last given param
 def case(selector, *args):
-    index = 0
+    index = -1
     selector_type = type(selector)
 
     if selector_type == bool:
-        index = 1 if selector else 0
+        index = 0 if selector else 1
     elif selector_type == int:
-        index = int(selector)
+        index = int(selector)  # make sure we have an int for table access
     else:
         print("Only booleans and numbers are supported as case selectors! Defaulting to last argument... Type: " + str(selector_type), file=sys.stderr)
 
-    return args[index] if index < len(args) else args[-1]
+    return args[index] if index >= 0 and index < len(args) else args[-1] # fetch value from given index or from the last index
+
+
+def _random(a=None, b=None):
+    if a == None and b == None:
+        return random.random()
+    else:
+        if b == None:
+            return random.randint(1,a)
+        else:
+            return random.randint(a,b)
 
 
 def smoothstep(x):
@@ -57,33 +77,49 @@ def smootheststep(x):
 
 
 def smoothmin(a, b, k):
-  k = k or 0.1
+  k = k if k != None else 0.1
   h = max(min(0.5+(b-a)/k, 1), 0)
   return h*a + (1-h) * (b - h*k*0.5)
 
 
 context = {
-    '__builtins__': None,
-    'round': round,
-    'square': lambda x: x ** 2,
-    'clamp': lambda x, mn, mx: min(max(x, mn), mx),
-    'smoothstep': smoothstep,
-    'smootherstep': smootherstep,
-    'smoothmin': smoothmin,
-    'sign': lambda x: max(min((x * 1e200) * 1e200, 1), -1),
-    'case': case,
-    'print': lambda val, label="": print(f'{label} = {val}' if label else str(val)),
+    '__builtins__': {
+        #0: Truth(0, True),
+        #'': Truth('', True),
+        'locals': locals,
+        'round': round,
+        'square': lambda x: x ** 2,
+        'clamp': lambda x, mn, mx: min(max(x, mn), mx),
+        'smoothstep': smoothstep,
+        'smootherstep': smootherstep,
+        'smoothmin': smoothmin,
+        'sign': lambda x: max(min((x * 1e200) * 1e200, 1), -1),
+        'case': case,
+        'random': _random,
+        'print': lambda val, label: print(f'{label} = {val}' if label != None else str(val)),
+    }
 }
 for k, v in math.__dict__.items():
-    context[k] = v
+    context['__builtins__'][k] = v
+
+
+# Define a function to convert variables in the expression to the desired format
+def convert_variable(match):
+    variable_name = match.group(1)
+    return 'locals().get("var_' + variable_name + '")'
 
 
 def parse_safe(expr: str, vars: dict):
     # Strip leading "$=" from expression
-    # replace all occurrences of "$" with "var_" (as these are used for Python variable names)
-    # replace nil with None
-    # replace ~= with !=
-    expr = expr[2:].replace('$', 'var_').replace('nil', 'None').replace('~=', '!=')
+    expr = expr[2:]
+
+    # Use regular expressions to find and replace variables in the expression to format that doesn't throw errors if variable doesn't exist
+    expr = re.sub(var_wrapper_re, convert_variable, expr)
+
+    # Convert Lua keywords to Python
+    expr = expr.replace('nil', 'None').replace('~=', '!=').replace('true', 'True').replace('false', 'False')
+
+    #print(expr)
 
     new_vars = {}
     for k,v in vars.items():
@@ -108,17 +144,74 @@ if __name__ == "__main__":
     vars = {'$toe_FR': 1, '$steer_center_F': 0.002}
     expr = "$=$toe_FR-$steer_center_F"
     result = parse_safe(expr, vars)
-    print(repr(result))  # 0.998
+    assert result == 0.998
 
-    '''vars = {'$springheight_F': 0.04}
+    vars = {'$springheight_F': 0.04}
     expr = "$=case($rideheight_F == nil, ($springheight_F + 0.09) * 0.7, '')"
     result = parse_safe(expr, vars)
-    print(repr(result)) # 0.091
+    assert result == 0.091
 
-    expr = "case(true, 1, 2)"
+    vars = {'$rideheight_F': 0.1, '$springheight_F': 0.04}
+    expr = "$=case($rideheight_F == nil, ($springheight_F + 0.09) * 0.7, '')"
     result = parse_safe(expr, vars)
-    print(repr(result))  # Output: 1
+    assert result == ''
 
-    expr = "case(false, 1, 2)"
+    vars = {'$brakestrength': 0.7}
+    expr = "$=$brakebias == nil and $brakestrength*900 or ($brakestrength*3600*(1-$brakebias) + 1)"
     result = parse_safe(expr, vars)
-    print(repr(result))  # Output: 2'''
+    assert result == 630
+    
+    # Doesn't pass but not using zero at the moment in logical statements so...
+    '''vars = {'$brakestrength': 0}
+    expr = "$=$brakebias == nil and $brakestrength*900 or ($brakestrength*3600*(1-$brakebias) + 1)"
+    result = parse_safe(expr, vars)
+    assert result == 0'''
+
+    vars = {'$brakebias': 0.5, '$brakestrength': 0.7}
+    expr = "$=$brakebias == nil and $brakestrength*900 or ($brakestrength*3600*(1-$brakebias) + 1)"
+    result = parse_safe(expr, vars)
+    assert result == 1261
+
+    expr = "$=case(true, 1, 2)"
+    result = parse_safe(expr, vars)
+    assert result == 1
+
+    expr = "$=case(false, 1, 2)"
+    result = parse_safe(expr, vars)
+    assert result == 2
+
+    expr = "$=random()"
+    result = parse_safe(expr, vars)
+    print("$=random(): " + str(result))
+
+    expr = "$=random(4)"
+    result = parse_safe(expr, vars)
+    print("$=random(4): " + str(result))
+
+    expr = "$=random(10,20)"
+    result = parse_safe(expr, vars)
+    print("$=random(10,20): " + str(result))
+
+    expr = "$=random(20,20)"
+    result = parse_safe(expr, vars)
+    assert result == 20
+
+    expr = "$=round(4)"
+    result = parse_safe(expr, vars)
+    assert result == 4
+
+    expr = "$=round(11.4)"
+    result = parse_safe(expr, vars)
+    assert result == 11
+
+    expr = "$=round(11.6)"
+    result = parse_safe(expr, vars)
+    assert result == 12
+
+    expr = "$=sign(1000)"
+    result = parse_safe(expr, vars)
+    assert result == 1
+
+    expr = "$=sign(-13)"
+    result = parse_safe(expr, vars)
+    assert result == -1
