7b350135ecb48581e81ef5b14ed096d66eae2d8a
amatteo <amatteo@beamng.com>
2024-01-26 15:13:31 -0800

    more optimizations

--- jbeam_editor/jbeam/variables.py
+++ jbeam_editor/jbeam/variables.py
@@ -19,15 +19,15 @@
 # SOFTWARE.
 
 import copy
-import re
+from re import compile as re_compile, search as re_search
 import sys
 
-from . import expression_parser
-from . import table_schema as jbeam_table_schema
-from . import utils as jbeam_utils
-from .. import utils
+from .expression_parser import parse_safe as expr_parse_safe
+from .table_schema import process_table_with_schema_destructive
+from .utils import Metadata
+from ..utils import row_dict_deepcopy, ipairs, clamp
 
-debug_parts = False  # set this to True to dump the parts to disk for manual inspection
+var_re = re_compile(r'(.*)\.(.*)')
 
 
 def apply(data: dict, variables: dict | None):
@@ -38,7 +38,7 @@ def apply(data: dict, variables: dict | None):
     while stackidx > 0:
         stackidx -= 1
         d: list | dict = stack[stackidx]
-        for key, v in (enumerate(list(d)) if isinstance(d, list) else list(dict(d).items())):
+        for key, v in (enumerate(list(d)) if isinstance(d, list) else list(d.items())):
             if isinstance(v, str):
                 if len(v) >= 2:
                     if ord(v[0]) == 36:  # $
@@ -47,16 +47,16 @@ def apply(data: dict, variables: dict | None):
                             if isinstance(d, list):
                                 if not isinstance(d[-1], dict):
                                     d.append({})
-                                if jbeam_utils.Metadata not in d[-1]:
-                                    d[-1][jbeam_utils.Metadata] = jbeam_utils.Metadata()
-                                metadata = d[-1][jbeam_utils.Metadata]
+                                if Metadata not in d[-1]:
+                                    d[-1][Metadata] = Metadata()
+                                metadata = d[-1][Metadata]
                                 metadata.set(key, 'expression', v)
                             else:
-                                if jbeam_utils.Metadata not in d:
-                                    d[jbeam_utils.Metadata] = jbeam_utils.Metadata()
-                                metadata = d[jbeam_utils.Metadata]
+                                if Metadata not in d:
+                                    d[Metadata] = Metadata()
+                                metadata = d[Metadata]
                                 metadata.set(key, 'expression', v)
-                            res_code, d[key] = expression_parser.parse_safe(v, variables)
+                            res_code, d[key] = expr_parse_safe(v, variables)
                         else:
                             if second_char not in (43, 60, 62): # +, <, >
                                 if v not in variables:
@@ -77,8 +77,8 @@ def apply(data: dict, variables: dict | None):
 def apply_slot_vars(slot_vars: dict, _variables: dict | None):
     # processes the slot variables repeatedly until they are all resolved:
     if _variables is None or len(_variables) == 0:
-        return utils.row_dict_deepcopy(slot_vars)
-    variables = utils.row_dict_deepcopy(_variables)
+        return row_dict_deepcopy(slot_vars)
+    variables = row_dict_deepcopy(_variables)
     succeed = {}
     for iters in range(1, 401):
         passed = False
@@ -87,7 +87,7 @@ def apply_slot_vars(slot_vars: dict, _variables: dict | None):
             if ord(v[0]) == 36:  # $
                 second_char = ord(v[1])
                 if second_char == 61:  # =
-                    res_code, res = expression_parser.parse_safe(v, variables)
+                    res_code, res = expr_parse_safe(v, variables)
                     if res_code == 0:
                         passed = True
                         succeed[k] = res
@@ -116,12 +116,12 @@ def apply_slot_vars(slot_vars: dict, _variables: dict | None):
             break
     if len(slot_vars) > 0:
         for k, v in slot_vars.items():
-            res_code, succeed[k] = expression_parser.parse_safe(v, variables)
+            res_code, succeed[k] = expr_parse_safe(v, variables)
     return succeed
 
 
 def _sanitize_vars(all_variables: dict, user_vars: dict):
-    variables = utils.row_dict_deepcopy(user_vars) # if var is present in config but not in the parts, still define them properly
+    variables = row_dict_deepcopy(user_vars) # if var is present in config but not in the parts, still define them properly
     for kv in list(all_variables.keys()):
         vv = all_variables[kv]
         if vv.get('type') == 'range':
@@ -169,13 +169,13 @@ def _sanitize_vars(all_variables: dict, user_vars: dict):
             if 'category' not in vv or vv['category'] == '':
                 vv['category'] = 'alignment'
 
-            regex = re.search(r'(.*)\.(.*)', vv['category'])
+            regex = re_search(var_re, vv['category'])
             if regex is not None:
                 vv['category'], vv['subCategory'] = regex.group(1), regex.group(2)
 
             # Make sure our value is actually inside the min/max limits
             # we can't be sure that "min" is actually the smaller number and "max" the bigger one, so for clamping we need to find out which is which first
-            vv['val'] = utils.clamp(vv['val'], min(vv['min'], vv['max']), max(vv['min'], vv['max']))
+            vv['val'] = clamp(vv['val'], min(vv['min'], vv['max']), max(vv['min'], vv['max']))
             variables[vv['name']] = vv
 
         else:
@@ -188,13 +188,13 @@ def _get_part_variables_parsing_variables_section_destructive(part: dict):
     res = {}
     if not isinstance(part.get('variables'), list):
         return {}
-    new_list_size = jbeam_table_schema.process_table_with_schema_destructive(part['variables'], res)
+    new_list_size = process_table_with_schema_destructive(part['variables'], res)
     return res
 
 
 def var_merge(dictionary: dict, dest: dict, src: dict):
     dest_end = len(dest)
-    for _, v in utils.ipairs(src):
+    for _, v in ipairs(src):
         place_idx = None
         if dictionary.get(v['name']):
             place_idx = dictionary[v['name']]
@@ -218,15 +218,9 @@ def process_parts(root_part: dict, unify_journal: list, vehicle_config: dict):
     for i in range(len(unify_journal) - 1, 0, -1):
         parent_part, part, level, slot_options, path, slot = unify_journal[i]
         slot_vars = slot.get('variables')
-        slot_id = slot['name'] if 'name' in slot else slot.get('type')
-
         if slot_vars is None:
             slot_vars = {}
         svars = apply_slot_vars(slot_vars, var_stack[id(parent_part)])
-
-        part_orig = None
-        if debug_parts:
-            part_orig = copy.deepcopy(part)
 
         temp = copy.deepcopy(var_stack[id(parent_part)])
         temp.update(svars)
@@ -235,9 +229,6 @@ def process_parts(root_part: dict, unify_journal: list, vehicle_config: dict):
         var_stack[id(part)] = svars
         apply(slot_options, svars) # nodeoffset
         apply(part, svars) # part
-        if debug_parts:
-            #json_write_file(f'{slot_id}.json', {'partPost': part, 'partPre': part_orig, 'slotvars': svars, 'slotOptions': slot_options}, True)
-            pass
 
     return variables
 
