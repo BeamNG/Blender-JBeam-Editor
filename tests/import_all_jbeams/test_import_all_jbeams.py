8bd164c7f88f3e884f35e9a75fbcfe46bef7b7a0
amatteo <amatteo@beamng.com>
2024-02-22 21:36:15 -0800

    added test to import all jbeam files

---
+++ tests/import_all_jbeams/test_import_all_jbeams.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2023 BeamNG GmbH, Angelo Matteo
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import bpy
+import bmesh
+
+import os
+import shutil
+from pathlib import Path
+
+from jbeam_editor import constants
+
+import pytest
+
+@pytest.mark.skip
+def test_1():
+    # Specify 'vehicles' folder path
+    vehicles_path = None
+
+    for dirpath, dirnames, filenames in os.walk(vehicles_path):
+        for filename in filenames:
+            if filename.endswith('.jbeam'):
+                full_path = f'{dirpath}\\{filename}'
+                assert bpy.ops.jbeam_editor.import_jbeam(filepath=full_path,import_all_parts=True) == {'FINISHED'}
+
+                collection: bpy.types.Collection = bpy.data.collections.get('JBeam Objects')
+                for obj in collection.objects:
+                    bpy.data.objects.remove(obj, do_unlink=True)
+                bpy.data.collections.remove(collection)
